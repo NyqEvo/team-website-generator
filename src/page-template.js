5d9e0ebcd267a9f7b4cbe9fb1e8eff823e7427a5
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2023-01-07 15:50:07 -0500

    added tests to all files in __test__ folder

--- src/page-template.js
+++ src/page-template.js
@@ -1,6 +1,19 @@
 module.exports = team => {
     const createManagerHtml = manager => {
-        return `<div class='manager'>
+        return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="Description" content="Enter your description here"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Title</title>
+        </head>
+        <body>
+        <div class='manager'>
         <h2>${manager.getName()}</h2>
         <p> Role: ${manager.getRole()}</p>
         <p> ID: ${manager.getId()}</p>
@@ -49,5 +62,9 @@ module.exports = team => {
         }
     })
 
-    return html.join('')
+    return html.push(`<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+    </body>
+    </html>`).join('')
 }
